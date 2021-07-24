fd6a57f2068cc748e8776e98ef4950437849e858
Jorge Eduardo Ardila <jeam@tuta.io>
2021-07-24 15:34:34 -0500

    feat: created schema

---
+++ start/server/src/schema.js
@@ -0,0 +1,43 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+
+	type Launch {
+		id: ID!
+		site:String
+		mission: Mission
+		rocket: Rocket
+		isBooked: Boolean!
+	}
+
+	type User = {
+		id: ID!
+		email: String!
+		trips: [Launch]!
+		token: String
+	}
+
+	type Mission = {
+		name: String
+		missionPatch(size: PatchSize): String
+	}
+
+	enum PatchSize {
+		SMALL
+		LARGE
+	}
+
+	type Query {
+		launches:[Launch]!
+		launch(id: ID!): Launch
+		me: User
+	}
+
+	type Mutation {
+		bookTrips(launchIds: [ID]!): TripUpdateResponse!
+		cancelTrip(launchId: ID!): TripUpdateResponse!
+		login(email:String): User
+	}
+`;
+
+module.exports typeDefs;
